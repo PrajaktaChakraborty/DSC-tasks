3e53c7d0d0c721c1c43b58e164c33cb485be2ae0
PrajaktaChakraborty <prajakta.chakraborty2020@gmail.com>
2024-10-05 21:45:29 +0530

    Array.py

--- Array.py
+++ Array.py
@@ -1,13 +1,10 @@
 def remove_duplicates(sorted_array):
-    # Initialize an empty list to store the result
     result = []
-    
-    # Loop through the sorted array
+
     for i in range(len(sorted_array)):
-        # If the result is empty or the current element is not equal to the last one added to the result
         if len(result) == 0 or sorted_array[i] != result[-1]:
-            result.append(sorted_array[i])  # Add the current element to the result
+            result.append(sorted_array[i])
     
     return result
-array=[1,2,3,3,5,5,6,6,6,6,7,8,8,9]
+array=[2,3,4,4,6,7,7]
 print(remove_duplicates(array))
